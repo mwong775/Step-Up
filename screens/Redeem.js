9894b0dbad4cb0e540baddecfe0b478bd7ecc111
mwong775 <mwong775@gmail.com>
2020-01-18 14:45:01 -0800

    add points increment with every 5 steps

---
+++ screens/Redeem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Redeem extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Redeem screen works!</Text>
+            </View>
+        );
+    }
+}
