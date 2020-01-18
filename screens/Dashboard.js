e172fe15dc16bc53ab7c43ec8142b35acc0159b4
mwong775 <mwong775@gmail.com>
2020-01-18 05:52:05 -0800

    add testing buttons

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Pedometer } from "expo-sensors";
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
     isPedometerAvailable: "checking",
     pastStepCount: 0,
     currentStepCount: 0,
+    currentPoints: 0,
     // below for tracking more stats eventually 
     totalPointsEarned: 0,
     totalSteps: 0,
@@ -62,26 +63,70 @@ export default class App extends React.Component {
     this._subscription = null;
   };
 
+  test() {
+    alert("test function works!");
+  }
+
+  _setPoints = () => {
+    // TODO: subscribe or something to trigger event and auto compute
+    // FORMULA FOR CALCULATING POINTS HERE
+    this.setState(prevState => ({currentPoints: prevState.currentPoints + prevState.currentStepCount/10}));
+  }
+
+  _addSteps = () => {
+    this.setState(prevState => ({ currentStepCount: prevState.currentStepCount + 1 }));
+  }
+
+  _addPoints = () => {
+    this.setState(prevState => ({ currentPoints: prevState.currentPoints + 1}));
+  }
+
+  _resetStats = () => {
+    this.setState(prevState => ({ 
+      currentStepCount: 0,
+      currentPoints: 0,
+    }));
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} style={styles.contentContainer}>
-          <Text style={styles.currentStepText}>Current Steps: {this.state.currentStepCount}</Text>
+          <Text style={styles.mainStatsText}>My Points: {this.state.currentPoints}</Text>
+          <Text style={styles.mainStatsText}>Current Steps: {this.state.currentStepCount}</Text>
           <Text style={styles.pastStepText}>
             Steps taken in the last 24 hours: {this.state.pastStepCount}
           </Text>
           <Text style={styles.asyncText}>
             Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}
           </Text>
+          <Button
+          style={styles.button}
+          onPress={this.test}
+          title="Test Button"
+          />
+          <Button
+          style={styles.button}
+          onPress={ () => this._addSteps() }
+          title="Add Steps"
+          />
+          <Button
+          style={styles.button}
+          onPress={ () => this._addPoints() }
+          title="Add Points"
+          />
+          <Button
+          style={styles.button}
+          onPress={ () => this._resetStats() }
+          title="Reset Stats"
+          />
         </ScrollView>
         <Image
             source={
                 require('../assets/images/footprint.jpg')
             }
             style={styles.footprintImage}
-          />
-
-
+        />
       </View>
     );
   }
@@ -95,7 +140,7 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingTop: 30,
   },
-  currentStepText: {
+  mainStatsText: {
     fontSize: 30, 
     textAlign: 'center',
   },
@@ -114,5 +159,5 @@ const styles = StyleSheet.create({
     height: '100%',
     zIndex: -1,
     opacity: 0.4,
-  }
+  },
 });
