a7ec9fe1b10f6f72db65d3fbc39a972ff253fb68
mwong775 <mwong775@gmail.com>
2020-01-19 09:25:50 -0800

    google nearby places api yeeeeeeeeee

--- screens/Map.js
+++ screens/Map.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import { StyleSheet, Dimensions, View, Text, FlatList } from 'react-native';
-// import Radar from 'react-native-radar';
+import { GOOGLE_API_KEY } from "react-native-dotenv";
 
 export default class Map extends React.Component {
   /**
@@ -14,35 +14,62 @@ export default class Map extends React.Component {
     super(props);
     this.state ={ 
       isLoading: true,
-      latitude: null,
-      longitude: null,
-      error:null,
-      markers: [{
-        coordinate: {
-          "latitude": 36.9975221,
-          "longitude": -122.0544869,
-        },
-        title: "test",
-      }]
+      lat: null,
+      long: null,
+      error: null,
+      places: [],
+      placeType: "park",
     }
   }
 
   componentDidMount(){
-    // return fetch('https://facebook.github.io/react-native/movies.json')
-    //   .then((response) => response.json())
-    //   .then((responseJson) => {
+    console.log(this.props);
+    console.log(this.state);
+    const { navigation } = this.props;
+    this.getCurrentLocation();
+  }
 
-    //     this.setState({
-    //       isLoading: false,
-    //       dataSource: responseJson.movies,
-    //     }, function(){
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      console.log("CURRENT LOCATION", lat, long);
+      this.setState({
+        lat: lat, long: long
+      });
+      this.getPlaces();
+    })
+  }
 
-    //     });
+  getPlaces() {
+    const { lat, long, placeType } = this.state;
+    const markers = [];
+    const url = this.getPlacesUrl(lat, long, 1500, placeType, GOOGLE_API_KEY);
+    console.log("URL", url);
+    fetch(url).then(res => res.json()).then(res => {
+      console.log("THIS RES THING", res);  
+      res.results.map((element, index) => {
+          const marketObj = {};
+          marketObj.id = element.id;
+          marketObj.name = element.name;
+          marketObj.marker = {
+            latitude: element.geometry.location.lat,
+            longitude: element.geometry.location.lng
+          };
+          console.log("MARKERS", markers);
+          markers.push(marketObj);
+        });
+        this.setState({places: markers});
+      });
+      console.log("GOT PLACES", this.state);
+  }
 
-    //   })
-    //   .catch((error) =>{
-    //     console.error(error);
-    //   });
+  getPlacesUrl(lat, long, radius, type, apiKey) {
+    const baseUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
+    const location = `location=${lat},${long}&radius=${radius}`;
+    const typeData = `&types=park`;
+    const api = `&key=${apiKey}`;
+    return `${baseUrl}${location}${typeData}${api}`;
   }
   
 
@@ -51,6 +78,7 @@ export default class Map extends React.Component {
       <View style={styles.container}>
       <MapView
         style={styles.map}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 36.9969384062279,
           longitude: -122.05221132051499,
@@ -58,16 +86,16 @@ export default class Map extends React.Component {
           longitudeDelta: 0.0421,
         }}
         >
-             {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
-         coordinate={{"latitude":this.state.latitude,"longitude":this.state.longitude}}
-         title={"Your Location"}
-       />}
-       {this.state.markers.map(marker => (
-    <Marker
-      coordinate={marker.coordinate}
-      title={marker.title}
-    />
-  ))}
+        {this.state.places.map((marker, i) => (
+          <MapView.Marker
+            key={i}
+            coordinate={{
+              latitude: marker.marker.latitude,
+              longitude: marker.marker.longitude
+            }}
+            title={marker.name}
+            />
+        ))}
         </MapView>
       </View>
     );
@@ -89,7 +117,3 @@ const styles = StyleSheet.create({
   textAlign: 'center',
  }
 });
-
-// SettingsScreen.navigationOptions = {
-//   title: 'app.json',
-// };
